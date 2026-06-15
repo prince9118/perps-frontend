6371dbfe430aee205b3c508c6de0684ffd88e04a
prince9118 <pchaudharyskn@gmail.com>
2026-06-15 12:27:46 +0530

    market components

---
+++ components/market/MarketCard.tsx
@@ -0,0 +1,14 @@
+type marketCardProps={
+    symbol:string;
+    price:number;
+    change:number;
+};
+
+export function MarketCard({symbol,price,change}:marketCardProps){
+    return (
+        <div>
+            <h2>{symbol}</h2>
+            <p>${price}</p>
+        </div>
+    )
+}
